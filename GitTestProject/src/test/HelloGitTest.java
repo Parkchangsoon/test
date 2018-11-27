1dde135048513291f84770e0afc9f42020cf4b7e
PCS <PCS@DESKTOP-R3UGP3G>
2018-11-27 14:36:33 +0900

    다시하기

--- GitTestProject/src/test/HelloGitTest.java
+++ GitTestProject/src/test/HelloGitTest.java
@@ -6,9 +6,6 @@ public class HelloGitTest {
 		// TODO Auto-generated method stub
 		
 		System.out.print("Hello Git Test!");
-		System.out.print("crate test1 branch");
-		System.out.print("마스터 브랜치에서 번경사항 출력");
-		System.out.print("test 브랜치에서 번경사항 출력");
 	}
 
 }
