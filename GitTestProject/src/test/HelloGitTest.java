608c071f9d79048608cc3c0a3bd6b25239163062
PCS <PCS@DESKTOP-R3UGP3G>
2018-11-27 14:43:09 +0900

    각 브랜치의 독립성 확인(branch1 에서)

--- GitTestProject/src/test/HelloGitTest.java
+++ GitTestProject/src/test/HelloGitTest.java
@@ -5,8 +5,9 @@ public class HelloGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Hello Git Test!");
-		System.out.print("브랜치1생성");
+		System.out.println("Hello Git Test!");
+		System.out.println("브랜치1생성");
+		System.out.println("각 브랜치의 독립성 확인(branch1 에서)");
 		
 	}
 
